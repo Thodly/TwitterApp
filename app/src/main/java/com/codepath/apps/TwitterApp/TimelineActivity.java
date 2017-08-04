4b679acb47683d41b41b9e9a9b1a92b7cfdc33ed
Thodly <thodlydugue@gmail.com>
2017-08-04 17:39:29 -0400

    update

---
+++ app/src/main/java/com/codepath/apps/TwitterApp/TimelineActivity.java
@@ -0,0 +1,65 @@
+package com.codepath.apps.TwitterApp;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ListView;
+
+import com.codepath.apps.TwitterApp.models.Tweet;
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+import cz.msebera.android.httpclient.Header;
+
+public class TimelineActivity extends ActionBarActivity {
+private TwitterClient client;
+    private TweetsArrayAdapter aTweets;
+    private ArrayList<Tweet> tweets;
+    private ListView lvTweets;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_timeline);
+        lvTweets = (ListView) findViewById(R.id.lvTweets);
+        tweets = new ArrayList<>();
+        aTweets = new TweetsArrayAdapter(this, tweets);
+        lvTweets.setAdapter((aTweets));
+
+        client = TwitterApplication.getRestClient();
+       populateTimeline();
+    }
+    private void populateTimeline(){
+        client.getHomeTimeline(1, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONArray json) {
+              Log.d("DEBUG",json.toString());
+                aTweets.addAll(Tweet.fromJSONArray(json));
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                Log.d("DEBUG", errorResponse.toString());
+            }
+        });
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_timeline, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+        if(id == R.id.action_settings){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
